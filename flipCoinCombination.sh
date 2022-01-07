2b8b9b55b0cd8c94511ad9a2ae38b592ef36bd51
Mayank <smayank748@gmail.com>
2022-01-07 09:04:48 +0530

    [Mayank]implementation of attendence percentage

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -2,14 +2,38 @@
 
 echo "Welcome to flip coin combination"
 
+declare -A flipCoin
 IS_HEAD=1
+counter=0
+headCount=0
+tailCount=0
+read -p "How many times you want to flip coin " noOfTime
 
-flipCoinResult=$((RANDOM%2))
-echo $flipCoinResult
+while [ $counter -lt $noOfTime ]
+do
 
-if [ $flipCoinResult -eq $IS_HEAD ]
-then
-	echo "Head"
-else
-	echo "Tail"
-fi
+	flipCoinResult=$((RANDOM%2))
+	echo $flipCoinResult
+
+	if [ $flipCoinResult -eq $IS_HEAD ]
+	then
+		echo "Head"
+		flipCoin[$counter]="Head"
+		#headCount=$headCount+1
+		((headCount++))
+	else
+		echo "Tail"
+		flipCoin[$counter]="Tail"
+		#tailCount=$tailCount+1
+		((tailCount++))
+	fi
+	((counter++))
+done 
+
+#Percentage of  head
+percnetageOfHead=$(((($headCount)*100)/$noOfTime))
+echo "Percentage of head " $percnetageOfHead
+
+#Percentage of tail
+percentageofTail=$((100-$percnetageOfHead))
+echo "Percentage of tail "$percentageofTail
