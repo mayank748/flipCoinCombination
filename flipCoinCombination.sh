bea9b74fe19a28441d3f92797dbdc3a06a03c4ea
Mayank <smayank748@gmail.com>
2022-01-07 08:45:25 +0530

    [Mayank]implemented to check head and tail

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -1,1 +1,15 @@
+#! /bin/bash -x
+
 echo "Welcome to flip coin combination"
+
+IS_HEAD=1
+
+flipCoinResult=$((RANDOM%2))
+echo $flipCoinResult
+
+if [ $flipCoinResult -eq $IS_HEAD ]
+then
+	echo "Head"
+else
+	echo "Tail"
+fi
